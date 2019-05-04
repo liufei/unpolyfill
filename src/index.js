7b3228f010f3d04c7365d7850b31e1ea29dbb115
biped <poticr@gmail.com>
2019-05-04 21:54:56 +0800

    For modern frontend

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'promise-polyfill/src/polyfill'
+import 'unfetch/polyfill'
+import objectAssign from 'object-assign'
+import 'core-js/features/symbol'
+import 'core-js/features/array/from'
+import 'core-js/features/map'
+import 'core-js/features/set'
+import 'core-js/features/url-search-params'
+import 'raf/polyfill'
+
+Object.assign = objectAssign
