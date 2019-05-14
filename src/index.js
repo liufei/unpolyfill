fe8dce7e3c855eaae191659524f404702c13eeba
biped <poticr@gmail.com>
2019-05-14 11:52:50 +0800

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-import 'promise-polyfill/src/polyfill'
-import 'unfetch/polyfill'
+import Promise from 'promise-polyfill'
+import setAsap from 'setasap'
+import Finally from 'promise-polyfill/src/finally'
+import fetch from 'unfetch'
 import objectAssign from 'object-assign'
 import 'core-js/features/symbol'
 import 'core-js/features/array/from'
 import 'core-js/features/map'
 import 'core-js/features/set'
 import 'core-js/features/url-search-params'
-import 'raf/polyfill'
+import raf from 'raf'
+
+if (typeof window.Promise === 'undefined') {
+  Promise._immediateFn = setAsap
+  Promise._unhandledRejectionFn = function(error) {
+    throw error
+  }
+  window.Promise = Promise
+}
+
+if (typeof window.Promise.prototype['finally'] === 'undefined') {
+  window.Promise.prototype['finally'] = Finally
+}
+
+if (typeof window.fetch === 'undefined') {
+  window.fetch = fetch
+}
 
 Object.assign = objectAssign
+
+if (typeof window !== 'undefined') {
+  raf.polyfill(window)
+}
